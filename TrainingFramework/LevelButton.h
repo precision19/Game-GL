c49fd8963ba006259c828115c978bd4ed859ce6d
xuongthin <xuongthin2000@gmail.com>
2021-08-31 09:55:03 +0700

    Update
    - Modify LevelButton class: sửa hành vi khi khoá
    
    - Modify LevelState.cpp: khi hoàn thành level 21 thì về map scene
    
    - Modify MenuBg.tga: thêm logo vào bg

--- TrainingFramework/LevelButton.h
+++ TrainingFramework/LevelButton.h
@@ -11,6 +11,7 @@ protected:
     int m_Star;
     Renderer* m_OnLockRenderer;
     Renderer* m_StarRenderer;
+    void Idle();
     virtual void OnPressing();
     virtual void OnClicked();
 public:
